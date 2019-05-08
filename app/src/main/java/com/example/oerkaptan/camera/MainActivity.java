373c624d9e1b670237c7a47cf135875ec537a37c
Okan Erkaptan <o.erkaptan@AD.SINGEN-HGS.LOGODIDACT.NET>
2019-05-08 14:42:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/oerkaptan/camera/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.oerkaptan.camera;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mResultEt;
+    ImageView mPreviewIv;
+
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 400;
+    private static final int IMAGE_PICK_GALLERY_CODE = 1000;
+    private static final int IMAGE_PICK_CAMERA_CODE = 1001;
+
+    String cameraPermission[];
+    String storagePermission[];
+
+    Uri image_uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mResultEt = findViewById(R.id.resultEt);
+        mPreviewIv = findViewById(R.id.imageIv);
+
+        //camera permission
+        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+
+    }
+
+
+
+
+    //menu
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id= item.getItemId();
+        if (id == R.id.addImage){
+            showImageImportdialog();
+        }
+        if (id == R.id.settings){
+            Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showImageImportdialog() {
+    String[] items = {"Camera", "Gallery"};
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Select Image");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if(which ==0 ) {
+//cam
+                    if (!checkCameraPermission()){
+                        requestCameraPermission();
+                    }
+                    else {
+                        pickCamera();
+                    }
+                }
+
+                if(which ==1 ) {
+                    if (!checkStoragePermission()){
+                        requestStoragePermission();
+                    }else {
+                        pickGallery();
+                    }
+//gallery
+                }
+            }
+        });
+        dialog.create().show();
+
+    }
+
+    private void requestCameraPermission() {
+        ActivityCompat
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
+
+}
+//https://www.youtube.com/watch?v=nc_Zmkf16kM
