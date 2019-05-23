44c843133d0f38025dfb5cf0f09a785f3fabf880
JDR3 <jannikdreier@web.de>
2019-05-23 20:09:00 +0200

    Schöner²

--- app/src/main/java/com/example/oerkaptan/camera/MainActivity.java
+++ app/src/main/java/com/example/oerkaptan/camera/MainActivity.java
@@ -28,6 +28,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
@@ -55,30 +56,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setSubtitle("Click + button to insert img");
+        actionBar.setSubtitle("Convert your Picture into Text");
 
         mResultEt = findViewById(R.id.resultEt);
         mPreviewIv = findViewById(R.id.imageIv);
         buttonCopy = findViewById(R.id.buttonCopy);
 
         //Kamera und Gallerie Permissions
-        cameraPermission = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
         storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
         buttonCopy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ClipboardManager clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clipData = ClipData.newPlainText("" ,mResultEt.getText().toString());
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("", mResultEt.getText().toString());
                 clipboardManager.setPrimaryClip(clipData);
                 Toast.makeText(getApplicationContext(), "Text kopiert", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-
-
-    //menu
+    //Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -87,52 +86,51 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id= item.getItemId();
-        if (id == R.id.addImage){
+        int id = item.getItemId();
+        if (id == R.id.addImage) {
             showImageImportdialog();
         }
-        if (id == R.id.settings){
+        if (id == R.id.settings) {
             Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
     }
 
     private void showImageImportdialog() {
-    String[] items = {"Camera", "Gallery"};
+        String[] items = {"Camera", "Gallery"};
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("Bild wählen");
         dialog.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int which) {
-                if(which ==0 ) {
-//cam
-                    if (!checkCameraPermission()){
+                if (which == 0) {
+                    if (!checkCameraPermission()) {
                         requestCameraPermission();
-                    }
-                    else {
+                    } else {
                         pickCamera();
                     }
                 }
 
-                if(which ==1 ) {
-                    if (!checkStoragePermission()){
+                if (which == 1) {
+                    if (!checkStoragePermission()) {
                         requestStoragePermission();
-                    }else {
+                    } else {
                         pickGallery();
                     }
-//gallery
                 }
             }
         });
         dialog.create().show();
     }
 
+    //Gallerie-Auswahl
     private void pickGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
         startActivityForResult(intent, IMAGE_PICK_GALLERY_CODE);
     }
 
+    //Kamera-Auswahl
     private void pickCamera() {
         ContentValues values = new ContentValues();
         values.put(MediaStore.Images.Media.TITLE, "NewPic");
@@ -142,10 +140,9 @@ public class MainActivity extends AppCompatActivity {
         image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,image_uri);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
         startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
     }
-
 
 
     private void requestStoragePermission() {
@@ -175,72 +172,72 @@ public class MainActivity extends AppCompatActivity {
     // permission result
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-       switch (requestCode){
-           case  CAMERA_REQUEST_CODE:
-               if (grantResults.length > 0){
-                   boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                   boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        switch (requestCode) {
+            case CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
 
-                   if (cameraAccepted && writeStorageAccepted){
-                       pickCamera();
-                   } else {
-                       Toast.makeText(this, "Zugriff verweigert", Toast.LENGTH_SHORT).show();
-                   }
-               }
-               break;
+                    if (cameraAccepted && writeStorageAccepted) {
+                        pickCamera();
+                    } else {
+                        Toast.makeText(this, "Zugriff verweigert", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
 
-           case STORAGE_REQUEST_CODE:
-               if (grantResults.length>0){
+            case STORAGE_REQUEST_CODE:
+                if (grantResults.length > 0) {
 
-                   boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
 
-                   if (writeStorageAccepted){
-                       pickGallery();
-                   } else {
-                       Toast.makeText(this, "Zugriff verweigert", Toast.LENGTH_SHORT).show();
-                   }
-               }
-               break;
-       }
+                    if (writeStorageAccepted) {
+                        pickGallery();
+                    } else {
+                        Toast.makeText(this, "Zugriff verweigert", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
     }
 
     //Handling der Bildauswahl
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
 
-        if (resultCode == RESULT_OK){
-            if (requestCode == IMAGE_PICK_GALLERY_CODE){
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_PICK_GALLERY_CODE) {
                 CropImage.activity(data.getData()).setGuidelines(CropImageView.Guidelines.ON)
                         .start(this);
             }
-            if (requestCode == IMAGE_PICK_CAMERA_CODE){
+            if (requestCode == IMAGE_PICK_CAMERA_CODE) {
                 CropImage.activity(image_uri).setGuidelines(CropImageView.Guidelines.ON)
                         .start(this);
             }
         }
 
         //Gecropptes Image übergeben
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK){
+            if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
 
                 mPreviewIv.setImageURI(resultUri);
 
-                BitmapDrawable bitmapDrawable = (BitmapDrawable)mPreviewIv.getDrawable();
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) mPreviewIv.getDrawable();
                 Bitmap bitmap = bitmapDrawable.getBitmap();
 
-                TextRecognizer recognizer = new  TextRecognizer.Builder(getApplicationContext()).build();
+                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
-                if (!recognizer.isOperational()){
+                if (!recognizer.isOperational()) {
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-                }else{
-                    Frame frame = new  Frame.Builder().setBitmap(bitmap).build();
+                } else {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
                     SparseArray<TextBlock> items = recognizer.detect(frame);
                     StringBuilder stringBuilder = new StringBuilder();
 
                     //Text vom StringBulder holen, bis kein Text mehr vorhanden ist
-                    for (int i = 0; i<items.size(); i++){
+                    for (int i = 0; i < items.size(); i++) {
                         TextBlock myItem = items.valueAt(i);
                         stringBuilder.append(myItem.getValue());
                         stringBuilder.append("\n");
@@ -248,11 +245,9 @@ public class MainActivity extends AppCompatActivity {
                     //Text im EditText anzeigen
                     mResultEt.setText(stringBuilder.toString());
                 }
-            }
-
-            else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE){
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 Exception error = result.getError();
-                Toast.makeText(this, ""+error, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
             }
         }
     }
